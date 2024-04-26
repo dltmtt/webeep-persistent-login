0f5acf75b35cf68aed40b0910fedb0191155d7fd
Matteo Delton <deltonmatteo@gmail.com>
2024-04-26 16:29:14 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const webeep_host = "webeep.polimi.it";
+const webeep_url = `https://${webeep_host}`;
+
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.create({ url: webeep_url });
+});
+
+chrome.webNavigation.onCompleted.addListener(
+  () => {
+    extendCookie(webeep_url, "MoodleSession");
+  },
+  { url: [{ hostEquals: webeep_host }] }
+);
+
+/**
+ * Extend the expiration date of a cookie by 400 days.
+ * @param {string} url the URL in which the cookie to extend is stored.
+ * @param {string} name the name of the cookie to extend.
+ */
+async function extendCookie(url, name) {
+  const cookie = await chrome.cookies.get({
+    url: url,
+    name: name,
+  });
+
+  if (cookie == null) {
+    return;
+  }
+
+  const now = new Date();
+  const in400days = new Date(now.setDate(now.getDate() + 400));
+
+  chrome.cookies.set({
+    url: url,
+    name: name,
+    value: cookie.value,
+    sameSite: cookie.sameSite,
+    secure: cookie.secure,
+    expirationDate: in400days.getTime() / 1000,
+  });
+
+  console.log(
+    chrome.i18n.getMessage("cookie_extended", [name, in400days.toISOString()])
+  );
+}
