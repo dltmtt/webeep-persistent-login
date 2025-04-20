e1afa50722c660cea84ab66e5939c73c4618406d
Matteo Delton <deltonmatteo@gmail.com>
2025-04-20 21:40:58 +0200

    Refactor email handling using sync storage

--- webex.js
+++ webex.js
@@ -1,14 +1,19 @@
-const emailInput = document.querySelector("#email");
+const hiddenEmailInput = document.querySelector("#email");
 const form = document.querySelector("#GlobalEmailLookupForm");
 
-let userEmail = localStorage.getItem("userEmail");
+chrome.storage.sync.get("email", (data) => {
+	let email = data.email;
 
-if (!userEmail) {
-	userEmail = prompt(
-		"Please enter your email. It will be stored locally for future use.",
-	);
-	localStorage.setItem("userEmail", userEmail);
-}
+	if (email) {
+		hiddenEmailInput.value = email;
+		form.submit();
+	}
 
-emailInput.value = userEmail;
-form.submit();
+	// If no email is found, get it from the input field and save it.
+	// Note that the form is submitted when the user clicks the button.
+	const loginButton = document.querySelector("#IDButton2");
+	loginButton.addEventListener("click", () => {
+		email = document.querySelector("#IDToken1").value;
+		chrome.storage.sync.set({ email });
+	});
+});
